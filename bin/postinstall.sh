a53a3b37328ab30e2842c04a9db48d67a0d09865
Jhen <developer@jhen.me>
2015-12-16 23:07:10 +0800

    Add check dev engines

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# link babel-preset-rn
 cd babel-preset-rn
 npm install
 cd ..
 npm link ./babel-preset-rn
+
+# check dev engines
+node node_modules/fbjs-scripts/node/check-dev-engines.js package.json
