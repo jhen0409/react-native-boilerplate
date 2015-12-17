9b2d13b8c23c07308b3a728748c6208417a7fcd9
Jhen <developer@jhen.me>
2015-12-17 22:52:38 +0800

    Add rnpm

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# check dev engines
+node node_modules/fbjs-scripts/node/check-dev-engines.js package.json
+
 # link babel-preset-rn
 cd babel-preset-rn
 npm install
 cd ..
 npm link ./babel-preset-rn
 
-# check dev engines
-node node_modules/fbjs-scripts/node/check-dev-engines.js package.json
+# rnpm link <module name>
