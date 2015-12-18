0dcbd03c8e5e07f80ea5ed0a1f19c3567479b848
Jhen <developer@jhen.me>
2015-12-18 21:04:09 +0800

    Fix newproject script

--- newproject.sh
+++ newproject.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-echo "Input your project name (EX: RNBoilerplate), followed by [ENTER]:"
+echo "Input your project name (like RNBoilerplate), followed by [ENTER]:"
 read project
 
 array=( package.json src test android ios )
 
 for i in "${array[@]}"
 do
-  replace 'RNBoilerplate' $project $i -r ;
+  # lowercase
   lc=$(echo "$project" | tr '[:upper:]' '[:lower:]') ;
-  replace 'rnboilerplate' $lc $i ;
+  # file content
+  replace 'RNBoilerplate' $project $i -r ;
+  replace 'rnboilerplate' $lc $i -r ;
 done
+
+mv ios/RNBoilerplate ios/$project
+mv ios/RNBoilerplate.xcodeproj ios/"$project".xcodeproj
+mv ios/RNBoilerplateTests/RNBoilerplateTests.m ios/RNBoilerplateTests/"$project"Tests.m
+mv ios/RNBoilerplateTests ios/"$project"Tests
+mv android/app/src/main/java/com/rnboilerplate android/app/src/main/java/com/$project
