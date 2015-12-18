da1f892b84b438096e539ea0da8c85a7f7ba58b7
Jhen <developer@jhen.me>
2015-12-18 20:34:47 +0800

    Split babel-preset-rn

--- postinstall.sh
+++ postinstall.sh
@@ -3,10 +3,4 @@
 # check dev engines
 node node_modules/fbjs-scripts/node/check-dev-engines.js package.json
 
-# link babel-preset-rn
-cd babel-preset-rn
-npm install
-cd ..
-npm link ./babel-preset-rn
-
 # rnpm link <module name>
