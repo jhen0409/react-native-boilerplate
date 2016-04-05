8520335efd59fa2fdac1402a4f0451ac107f1430
Jhen <developer@jhen.me>
2016-04-06 02:11:44 +0800

    Add jsdom setup for enzyme mount

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,10 @@
+import { jsdom } from 'jsdom';
+
 global.fetch = require('node-fetch');
 global.Immutable = require('immutable');
+global.document = jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = global.window.navigator;
+
 require('babel-polyfill');
 require('react-native-mock/mock');
